16e05370425b2a1e52106ba187114d6386cfcc59
Estefania Arciniegas Gutierrez <estefargu@MacBook-Air-de-Estefania.local>
2024-04-04 22:01:01 -0500

    configuracion app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React , {useState, useEffect}from 'react';
+import Axios from 'axios';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [contador, setContador] = useState(0); 
+  const [departamentos, setDepartamentos] = useState([]);
+  const [deptoSeleccionado, setDeptoSeleccionado] = useState('');
+  const [ciudades, setCiudades] = useState([]);
+  const [ciudadSeleccionada, setCiudadSeleccionada] = useState('');
+
+  useEffect(()=>{
+
+    const obtenerDeptos = async () => {
+      const response = await Axios({
+        url: 'http://localhost:1337/api/listdepartments'
+      });
+      const listDepartamentos = Object.keys(response.data).map(i => response.data[i]);
+      setDepartamentos(listDepartamentos.flat()); 
+    }
+
+    const obtenerCiudades = async (departmentId) => {
+      const response = await Axios ({
+        url:`http://localhost:1337/api/listcities/${departmentId}`
+      });
+      const listCiudades = Object.keys(response.data).map(i=> response.data[i]);
+      setCiudades(listCiudades.flat());
+    }
+
+    obtenerDeptos();
+
+    if(deptoSeleccionado !== "")
+      obtenerCiudades(deptoSeleccionado);
+  },[deptoSeleccionado]);
+
+  function handleClick(){
+    setContador(contador+1);
+  }
+  function handleDisminuirClick (){
+    setContador(contador - 1);
+  }
+
+  function handleDepartamentosSelect(event){
+    setDeptoSeleccionado(event.target.value);
+  }
+
+  function handleCiudadesSelect(event){
+    setCiudadSeleccionada(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <label>Contador {contador} </label>
+      <div>
+        <button onClick={handleClick} > Aumentar contador </button>
+      </div>
+      <div>
+        <button onClick={handleDisminuirClick} > Disminuir contador </button>
+      </div>
+      <div>
+        <select id = "opcionesDepartamentos" value= {deptoSeleccionado} onChange={handleDepartamentosSelect}>
+          <option value="">Seleccione un departamento</option>
+          {departamentos.map(opcion =>(
+          <opcion key={opcion.value} value={opcion.value}>{opcion.label}</opcion>
+          ))}
+        </select>
+      </div>
+      <div>
+        <select id = "opcionesCiudades" value= {ciudadSeleccionada} onChange={handleCiudadesSelect}>
+          <option value="">Seleccione una ciudad</option>
+          {ciudades.map(opcion =>(
+          <opcion key={opcion.value} value={opcion.value}>{opcion.label}</opcion>
+          ))}
+        </select>
+      </div>
+
     </div>
   );
 }
-
-export default App;
